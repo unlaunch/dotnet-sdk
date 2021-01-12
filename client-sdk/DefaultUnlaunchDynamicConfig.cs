1d604c285fbb6eae63fe1619632516d13467e998
Tuan Nguyen <tuan.nguyen@gettyimages.com>
2021-01-11 22:19:12 -0700

    Implment dynamic config

---
+++ client-sdk/DefaultUnlaunchDynamicConfig.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace io.unlaunch
+{
+    class DefaultUnlaunchDynamicConfig : UnlaunchDynamicConfig
+    {
+        private readonly IDictionary<string, string> _configMap;
+
+        DefaultUnlaunchDynamicConfig(IDictionary<string, string> configMap)
+        {
+            _configMap = configMap ?? new Dictionary<string, string>(1);
+        }
+
+        public bool ContainsKey(string key)
+        {
+            return _configMap.ContainsKey(key);
+        }
+
+        public bool GetBoolean(string key)
+        {
+            return _configMap[key].ToLower() == "true";
+        }
+
+        public bool GetBoolean(string key, bool defaultValue)
+        {
+            if (!_configMap.ContainsKey(key))
+            {
+                return defaultValue;
+            }
+
+            return _configMap[key].ToLower() == "true";
+        }
+
+        public double GetDouble(string key)
+        {
+            return double.Parse(_configMap[key]);
+        }
+
+        public double GetDouble(string key, double defaultValue)
+        {
+            try
+            {
+                return double.Parse(_configMap[key]);
+            }
+            catch
+            {
+                return defaultValue;
+            }
+        }
+
+        public float GetFloat(string key)
+        {
+            return float.Parse(_configMap[key]);
+        }
+
+        public float GetFloat(string key, float defaultValue)
+        {
+            try
+            {
+                return float.Parse(_configMap[key]);
+            }
+            catch
+            {
+                return defaultValue;
+            }
+        }
+
+        public int GetInt(string key)
+        {
+            return int.Parse(_configMap[key]);
+        }
+
+        public int GetInt(string key, int defaultValue)
+        {
+            try
+            {
+                return int.Parse(_configMap[key]);
+            }
+            catch
+            {
+                return defaultValue;
+            }
+        }
+
+        public long GetLong(string key)
+        {
+            return long.Parse(_configMap[key]);
+        }
+
+        public long GetLong(string key, long defaultValue)
+        {
+            try
+            {
+                return long.Parse(_configMap[key]);
+            }
+            catch
+            {
+                return defaultValue;
+            }
+        }
+
+        public string GetString(string key)
+        {
+            return _configMap[key];
+        }
+
+        public string GetString(string key, string defaultValue)
+        {
+            if (_configMap.ContainsKey(key))
+            {
+                return _configMap[key];
+            }
+
+            return defaultValue;
+        }
+
+        public bool IsEmpty()
+        {
+            return !_configMap.Any();
+        }
+
+        public bool IsAny()
+        {
+            return _configMap.Any();
+        }
+
+        public int Size()
+        {
+            return _configMap.Count;
+        }
+
+        public int Count()
+        {
+            return _configMap.Count;
+        }
+    }
+}
---
+++ client-sdk/UnlaunchDynamicConfig.cs
@@ -0,0 +1,40 @@
+﻿namespace io.unlaunch
+{
+    interface UnlaunchDynamicConfig
+    {
+        bool ContainsKey(string key);
+
+        bool GetBoolean(string key);
+
+        bool GetBoolean(string key, bool defaultValue);
+
+
+        double GetDouble(string key);
+
+        double GetDouble(string key, double defaultValue);
+
+        float GetFloat(string key);
+
+        float GetFloat(string key, float defaultValue);
+
+        int GetInt(string key);
+
+        int GetInt(string key, int defaultValue);
+
+        long GetLong(string key);
+
+        long GetLong(string key, long defaultValue);
+
+        string GetString(string key);
+
+        string GetString(string key, string defaultValue);
+
+        bool IsEmpty();
+
+        bool IsAny();
+
+        int Size();
+
+        int Count();
+    }
+}
